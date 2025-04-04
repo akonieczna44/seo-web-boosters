5fd8165138f71364e8253e988587ec83081e3c23
akonieczna44 <50301538+akonieczna44@users.noreply.github.com>
2025-04-04 22:53:33 +0200

    Create podmien-stare-nowe-logo.py

---
+++ desktop/podmien-stare-nowe-logo.py
@@ -0,0 +1,3 @@
+# podmiana starego + zamalowanie białym + wstawienie nowego + zamalowanie białym + wstawienie nowego
+# po podaniu folderu i ścieżki nowego i starego logo
+# pięknie rozpoznajesz, Py
